6ffd576e9fa02f3134c6607fb9b1154efa6c6d79
DevroDev <devroshein.dev@gmail.com>
2020-11-01 12:23:23 +0630

    auth and all route complete

---
+++ firebase/index.js
@@ -0,0 +1,11 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://shopify-3ca02.firebaseio.com"
+});
+
+module.exports=admin
